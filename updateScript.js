785eb76bb3836b27610f99a5580364de89034963
ModerNew <grzesiujag@poczta.fm>
2021-02-22 14:45:58 +0100

    added visibility config

--- updateScript.js
+++ updateScript.js
@@ -1,6 +1,12 @@
 function updateUser() {
     var configs = JSON.parse(data);
-    document.getElementById('name').innerHTML = configs[0].name;
-    document.getElementById('title').innerHTML = configs[0].title + " - " + configs[0].author;
+    if (configs[0].visibility == "true"){
+        document.getElementById('name').innerHTML = configs[0].name;
+        document.getElementById('title').innerHTML = configs[0].title + " - " + configs[0].author;
+    }else{
+        document.getElementById('name').innerHTML = " ";
+        document.getElementById('title').innerHTML = " ";
+    }
+    
     var t = setTimeout(updateUser, 500);
 }
