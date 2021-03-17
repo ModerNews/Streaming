c843aad385cc313107dbe2e9cf790d62d5c69ad8
ModerNew <grzesiujag@poczta.fm>
2021-03-17 13:46:20 +0100

    Adding Control Panel Project

---
+++ Stream Control Panel/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Stream_Control_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var sr = new StreamReader("G:\\Streaming\\data.js"))
+                {
+                    //var fullFile = sr.ReadToEnd();
+                    sr.ReadLine();
+                    String json_Data = sr.ReadLine();
+                    int n = 12;
+                    json_Data = json_Data.Substring(n, json_Data.Length - (n + 2));
+                    JObject json = JObject.Parse(json_Data);
+                    textBox1.Text = (string)json;
+                }
+            }
+            catch (IOException erorr)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(erorr.Message);
+            }
+        }
+    }
+}
